084f1a35e1da609149f42a3e5810bb721854f9f4
Mateusz <mateuszsuchocki7@gmail.com>
2021-03-19 11:51:59 +0100

    Writing proper logs

--- lib/Utils.cpp
+++ lib/Utils.cpp
@@ -6,6 +6,10 @@
 #include <memory>
 #include <sstream>
 #include <vector>
+#include <unistd.h>
+#include <csignal>
+#include <ftw.h>
+#include <syslog.h>
 
 static std::string exec(const char *cmd) {
     std::array<char, 128> buffer{};
@@ -30,3 +34,58 @@ static std::vector<std::string> split(const std::string &s, char delimiter) {
     return tokens;
 }
 
+static void skeleton_daemon() {
+    // Define variables
+    pid_t pid, sid;
+
+    // Fork the current process
+    pid = fork();
+    // The parent process continues with a process ID greater than 0
+    if(pid > 0)
+    {
+        exit(EXIT_SUCCESS);
+    }
+        // A process ID lower than 0 indicates a failure in either process
+    else if(pid < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+    // The parent process has now terminated, and the forked child process will continue
+    // (the pid of the child process was 0)
+
+    // Since the child process is a daemon, the umask needs to be set so files and logs can be written
+    umask(0);
+
+    // Open system logs for the child process
+    openlog("Avitex", LOG_NOWAIT | LOG_PID, LOG_USER);
+    syslog(LOG_NOTICE, "Successfully started Avitex");
+
+    // Generate a session ID for the child process
+    sid = setsid();
+    // Ensure a valid SID for the child process
+    if(sid < 0)
+    {
+        // Log failure and exit
+        syslog(LOG_ERR, "Could not generate session ID for child process");
+
+        // If a new session ID could not be generated, we must terminate the child process
+        // or it will be orphaned
+        exit(EXIT_FAILURE);
+    }
+
+    // Change the current working directory to a directory guaranteed to exist
+    if((chdir("/")) < 0)
+    {
+        // Log failure and exit
+        syslog(LOG_ERR, "Could not change working directory to /");
+
+        // If our guaranteed directory does not exist, terminate the child process to ensure
+        // the daemon has not been hijacked
+        exit(EXIT_FAILURE);
+    }
+
+    // A daemon cannot use the terminal, so close standard file descriptors for security reasons
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <array>
-#include <memory>
 #include <vector>
-#include <sstream>
+#include <unistd.h>
+#include <ftw.h>
 
 #include "lib/Utils.cpp"
 
@@ -30,36 +29,62 @@ int main() {
      *  - cache hashes to the file during first run (maybe divide into smaller files)
      *  - compare differed hash functions times
      */
-    // Vector of all files in given directory
-    std::vector<std::string> files = split(exec("find /home/mateusz/Desktop -type f -print0 | xargs -0 ls"), '\n');
-    std::ifstream virus_signatures;
 
-    //cache_hashes("/home");
+    skeleton_daemon();
 
-    virus_signatures.open("../v_signatures.txt");
+    // Daemon-specific initialization should go here
+    const int SLEEP_INTERVAL = 5;
 
-    std::cout << "Welcome to Avitex" << std::endl;
+    while(true)
+    {
+        // Execute daemon heartbeat, where your recurring activity occurs
+        // Vector of all files in given directory
+        std::vector<std::string> files = split(exec("find /home/mateusz/Desktop -type f -print0 | xargs -0 ls"), '\n');
+        std::ifstream virus_signatures;
 
-    if (virus_signatures.is_open()) {
-        std::string line;
-        while (std::getline(virus_signatures, line)) {
-            // Compares hashes from files found by find command with hashes in virus_signatures
-            for (auto & i : files) {
-                const std::string& file = i;
-                // Gets only hashes form md5sum command
-                std::string md5_command = "md5sum '" + file + "' | cut -f 1 -d \" \"";
-                std::string file_hash = exec(md5_command.c_str());
-                file_hash.pop_back(); // Remove eol char
+        // TODO: change to some dir with program (probably change child working dir)
+        virus_signatures.open("/home/mateusz/Desktop/Avitex/v_signatures.txt");
 
-                if (line == file_hash) {
-                    std::cout << "Virus found: " + i << std::endl;
+        if (virus_signatures.is_open()) {
+            std::string line;
+            bool virus_found = false;
+            while (std::getline(virus_signatures, line)) {
+                // Compares hashes from files found by find command with hashes in virus_signatures
+                for (auto &i : files) {
+                    const std::string &file = i;
+                    // Gets only hashes form md5sum command
+                    std::string md5_command = "md5sum '" + file + "' | cut -f 1 -d \" \"";
+                    std::string file_hash = exec(md5_command.c_str());
+                    file_hash.pop_back(); // Remove eol char
+
+                    if (line == file_hash) {
+                        virus_found = true;
+                        syslog(LOG_ALERT, "%s", ("Virus found: " + i).c_str());
+                    }
+
                 }
             }
+            if (!virus_found) {
+                syslog(LOG_NOTICE, "No viruses :)");
+            }
+            virus_signatures.close();
+            // This break makes daemon to run once
+            break;
+
+        } else {
+            syslog(LOG_ERR, "Cannot open signatures file");
+            break;
         }
-        virus_signatures.close();
-    } else {
-        std::cerr << "Error during opening a virus signatures file\n";
+
+        // Sleep for a period of time
+        sleep(SLEEP_INTERVAL);
+
     }
 
-    return 0;
+    // Close system logs for the child process
+    syslog(LOG_NOTICE, "Stopping Avitex");
+    closelog();
+
+    // Terminate the child process when the daemon completes
+    exit(EXIT_SUCCESS);
 }
