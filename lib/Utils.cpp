06ef237d9bab162884d98d67a58fa849492d5cab
Mateusz <mateuszsuchocki7@gmail.com>
2021-03-25 22:28:07 +0100

    Getting better and better

--- lib/Utils.cpp
+++ lib/Utils.cpp
@@ -10,6 +10,7 @@
 #include <csignal>
 #include <ftw.h>
 #include <syslog.h>
+#include <iostream>
 
 static std::string exec(const char *cmd) {
     std::array<char, 128> buffer{};
@@ -32,6 +33,59 @@ static std::vector<std::string> split(const std::string &s, char delimiter) {
         tokens.push_back(token);
     }
     return tokens;
+}
+
+static void skeleton_daemon2() {
+    pid_t pid;
+
+    /* Fork off the parent process */
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* On success: The child process becomes session leader */
+    if (setsid() < 0)
+        exit(EXIT_FAILURE);
+
+    /* Catch, ignore and handle signals */
+    //TODO: Implement a working signal handler */
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    /* Fork off for the second time*/
+    pid = fork();
+
+    /* An error occurred */
+    if (pid < 0)
+        exit(EXIT_FAILURE);
+
+    /* Success: Let the parent terminate */
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    /* Set new file permissions */
+    umask(0);
+
+    /* Change the working directory to the root directory */
+    /* or another appropriated directory */
+    chdir("/");
+
+    /* Close all open file descriptors */
+    int x;
+    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
+    {
+        close (x);
+    }
+
+    /* Open the log file */
+    openlog ("Avitex", LOG_PID, LOG_DAEMON);
 }
 
 static void skeleton_daemon() {
--- main.cpp
+++ main.cpp
@@ -7,19 +7,60 @@
 
 #include "lib/Utils.cpp"
 
-void cache_hashes(const std::string& dir) {
-    std::ofstream cache;
-    cache.open("md5sums.md5");
+//void cache_hashes(const std::string& dir) {
+//    std::ofstream cache;
+//    cache.open("md5sums.md5");
+//
+//    if (cache.is_open()) {
+//        std::string cmd = "find " + dir + " -type f -exec md5sum {} + > md5sums.md5";
+//        std::vector<std::string> hashes = split(exec(cmd.c_str()), '\n');
+//        for (auto &hash : hashes)  {
+//            cache << hash << '\n';
+//        }
+//        cache.close();
+//    } else {
+//        std::cout << "Cannot open cache file" << std::endl;
+//    }
+//}
 
-    if (cache.is_open()) {
-        std::string cmd = "find " + dir + " -type f -exec md5sum {} + > md5sums.md5";
-        std::vector<std::string> hashes = split(exec(cmd.c_str()), '\n');
-        for (auto &hash : hashes)  {
-            cache << hash << '\n';
+int scan() {
+    // Execute daemon heartbeat, where your recurring activity occurs
+    // Vector of all files in given directory
+    std::vector<std::string> files = split(exec("find /home/mateusz/Desktop -type f -print0 | xargs -0 ls"), '\n');
+    std::ifstream virus_signatures;
+
+    // TODO: change to some dir with program (probably change child working dir)
+    virus_signatures.open("/home/mateusz/Desktop/Avitex/v_signatures.txt");
+
+    if (virus_signatures.is_open()) {
+        std::string line;
+        bool virus_found = false;
+        while (std::getline(virus_signatures, line)) {
+            // Compares hashes from files found by find command with hashes in virus_signatures
+            for (auto &i : files) {
+                const std::string &file = i;
+                // Gets only hashes form md5sum command
+                std::string md5_command = "md5sum '" + file + "' | cut -f 1 -d \" \"";
+                std::string file_hash = exec(md5_command.c_str());
+                file_hash.pop_back(); // Remove eol char
+
+                if (line == file_hash) {
+                    virus_found = true;
+                    syslog(LOG_ALERT, "%s", ("Virus found: " + i).c_str());
+                }
+
+            }
         }
-        cache.close();
+        if (!virus_found) {
+            syslog(LOG_NOTICE, "No viruses :)");
+        }
+        virus_signatures.close();
+        // If return 1 than daemon run once
+        return 1;
+
     } else {
-        std::cout << "Cannot open cache file" << std::endl;
+        syslog(LOG_ERR, "Cannot open signatures file");
+        return 1;
     }
 }
 
@@ -30,50 +71,40 @@ int main() {
      *  - compare differed hash functions times
      */
 
-    skeleton_daemon();
+//    if (argc < 2) {
+//        std::cerr << "Type 'start' to start the program or 'stop' to terminate" << std::endl;
+//        return 1;
+//    }
+//
+//    if (std::string(argv[1]) != "start") {
+//        std::cerr << "Type 'start' to start the program or 'stop' to terminate" << std::endl;
+//        return 1;
+//    }
+
+    //skeleton_daemon2();
 
     // Daemon-specific initialization should go here
-    const int SLEEP_INTERVAL = 5;
+    const int SLEEP_INTERVAL = 1;
 
     while(true)
     {
-        // Execute daemon heartbeat, where your recurring activity occurs
-        // Vector of all files in given directory
-        std::vector<std::string> files = split(exec("find /home/mateusz/Desktop -type f -print0 | xargs -0 ls"), '\n');
-        std::ifstream virus_signatures;
+        // TODO: Implement some input handling (display statistics, start scanning, ...)
+        std::string a;
+        std::cout << "Type 'q' to exit and 'scan' to scan: ";
+        std::cin >> a;
+        if (a == "q") {
+            std::cout << "Press any key to quit" << std::endl;
+            return 0;
+        }
 
-        // TODO: change to some dir with program (probably change child working dir)
-        virus_signatures.open("/home/mateusz/Desktop/Avitex/v_signatures.txt");
+        std::cout << "Your input: " + a << std::endl;
 
-        if (virus_signatures.is_open()) {
-            std::string line;
-            bool virus_found = false;
-            while (std::getline(virus_signatures, line)) {
-                // Compares hashes from files found by find command with hashes in virus_signatures
-                for (auto &i : files) {
-                    const std::string &file = i;
-                    // Gets only hashes form md5sum command
-                    std::string md5_command = "md5sum '" + file + "' | cut -f 1 -d \" \"";
-                    std::string file_hash = exec(md5_command.c_str());
-                    file_hash.pop_back(); // Remove eol char
-
-                    if (line == file_hash) {
-                        virus_found = true;
-                        syslog(LOG_ALERT, "%s", ("Virus found: " + i).c_str());
-                    }
-
-                }
+        if (a == "scan") {
+            std::cout << "Scanning..." << std::endl;
+            // Exit code from scan (if 0 - loop and loop, any other loop once)
+            if (scan() != 0) {
+                std::cout << "Scan finished. Results can be found in /var/sys/syslog." << std::endl;
             }
-            if (!virus_found) {
-                syslog(LOG_NOTICE, "No viruses :)");
-            }
-            virus_signatures.close();
-            // This break makes daemon to run once
-            break;
-
-        } else {
-            syslog(LOG_ERR, "Cannot open signatures file");
-            break;
         }
 
         // Sleep for a period of time
